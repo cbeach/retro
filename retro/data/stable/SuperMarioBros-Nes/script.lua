aff254cee2622ac30326bfa6d6a9cb3867c874e6
Casey Beach <beachc@gmail.com>
2018-12-10 19:17:05 -0800

    update script in preparation for exp3

--- retro/data/stable/SuperMarioBros-Nes/script.lua
+++ retro/data/stable/SuperMarioBros-Nes/script.lua
@@ -1,5 +1,6 @@
 previous_score = 0
 previous_pos = 0
+previous_time = 0
 
 data.prev_lives = 3
 
@@ -22,17 +23,22 @@ function correct_reward ()
 
   local score = data.score * 10
   local pos = data.xscrollLo + data.xscrollHi * 256
-  -- print("lua score", score)
-  -- print("lua pos", pos)
   local delta_score = 0
   local delta_pos = 0
   local delta_lives = 0
   local scaled_delta_score = 0
-
+  local delta_time = 0
 
   delta_score = score - previous_score
   previous_score = score
   scaled_delta_score = delta_score / 10
+
+  if data.time > previous_time then
+    previous_time = data.time
+  elseif data.time < data. then
+    previous_time = data.time
+    delta_time = previous_time - data.time
+  end
 
   if pos > previous_pos then
     delta_pos = pos - previous_pos
@@ -40,7 +46,7 @@ function correct_reward ()
   end  
   -- print('pos', pos, 'previous_pos', previous_pos, 'd_pos', delta_pos, 'xscrollLo', data.xscrollLo, 'xscrollHi', data.xscrollHi)
 
-  -- delta_lives = (data.lives - data.prev_lives)
+  delta_lives = (data.lives - data.prev_lives)
 
-  return delta_pos + scaled_delta_score -- + delta_lives
+  return delta_pos + scaled_delta_score + delta_lives + delta_time
 end
